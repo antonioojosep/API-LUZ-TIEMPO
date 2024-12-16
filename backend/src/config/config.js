15b93510629c6b71982e7cb9e6195a2d1d558851
antonioojosep <antonillocarriillo@gmail.com>
2024-12-17 00:08:21 +0100

    jesus fallos

--- backend/src/config/config.js
+++
@@ -1,7 +0,0 @@
-import dotenv from "dotenv";
-
-dotenv.config();
-
-export const PORT = process.env.PORT || 3000;
-export const DATABASE_PATH = process.env.DATABASE_PATH || "backend/database/luz.sqlite";
-export const API_URL = process.env.API_URL || "https://apidatos.ree.es/es/datos/mercados/precios-mercados-tiempo-real?start_date=2024-01-01T00:00&end_date=2024-01-31T23:59&time_trunc=hour";
