22a37ae9773fd5eb2071fd46400413ef23e8600f
antonioojosep <antonillocarriillo@gmail.com>
2024-12-17 08:53:46 +0100

    cambio ruta

--- backend/src/routes/wheaterRouter.js
+++ backend/src/routes/wheaterRouter.js
@@ -8,3 +8,5 @@ router.get("/", (req, res) => {
     
     getWeather(city);
 })
+
+export default router;
--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ import express from 'express';
 import { PORT } from './config/config.js';
 import userRouter from './routes/userRouter.js';
 import daysRouter from './routes/daysRouter.js';
+import wheaterRouter from './routes/ wheaterRouter.js';
 
 const app = express();
 
@@ -11,6 +12,7 @@ app.use(cors());
 
 app.use('/api/users', userRouter);
 app.use('/api/days', daysRouter);
+app.use('/api/weather', wheaterRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
